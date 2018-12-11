c7b9241f01321a11a68ac4ad297b226a97c68890
wormhole1996 <363408268@qq.com>
2018-12-11 16:13:23 +0800

    更新webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                use: 'babel-loader',
+                loader: 'babel-loader',
                 exclude: /node_modules/
             },
             {
