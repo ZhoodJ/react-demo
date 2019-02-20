a103540a834bd2e21d032a7e9142a68efb590e2a
wormhole1996 <363408268@qq.com>
2019-02-20 14:06:50 +0800

    改进react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from "./component/HelloWorld.jsx";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./component/Home.jsx";
+import About from "./component/About.jsx";
+import {BrowserRouter, Route, Switch, Link} from "react-router-dom";
 
 ReactDOM.render((
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={HelloWorld}></Route>
-        </Switch>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+            <hr/>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+        </div>
     </BrowserRouter>
 ), document.getElementById("root"));
