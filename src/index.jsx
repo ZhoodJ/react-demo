4194cc313961fa02f7e79aadb400937fccecb684
wormhole1996 <363408268@qq.com>
2019-02-20 11:49:27 +0800

    整合react-router 4

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from "./component/HelloWorld.jsx";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-ReactDOM.render(<HelloWorld/>, document.getElementById("root"));
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={HelloWorld}></Route>
+        </Switch>
+    </BrowserRouter>
+), document.getElementById("root"));
