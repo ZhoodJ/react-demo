d79aec6cf26d63329555f4c42208cba5e2479954
wormhole1996 <363408268@qq.com>
2019-02-23 22:12:41 +0800

    登陆页面

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Layout} from 'antd';
+import {Button, Checkbox, Input, Layout} from 'antd';
 import "./Login.less";
 
 const {Content, Footer} = Layout;
@@ -9,13 +9,17 @@ class Login extends Component {
     render() {
 
         return (
-            <Layout style={{height: "100vh"}}>
-                <Content style={{padding: '50px', height: "100vh"}}>
-                    <div style={{background: '#fff', padding: 24, height: "100vh"}}>
-
+            <Layout className="layout">
+                <Content className="content">
+                    <div className="login-div">
+                        <Input size="large" type="email" placeholder="请输入邮箱" className="input"/>
+                        <Input size="large" type="password" placeholder="请输入密码" className="input"/>
+                        <Checkbox className="checkbox">记住密码</Checkbox>
+                        <br/>
+                        <Button type="primary" size="large" className="button">登陆</Button>
                     </div>
                 </Content>
-                <Footer style={{textAlign: 'center'}}>
+                <Footer className="footer">
                     copyright ©2018 by 凉衫薄
                 </Footer>
             </Layout>
